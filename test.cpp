ab2d22529a2b13c3a91e6b5c9a8aab960b8a1b95
lena <neostromag@yandex.ru>
2021-09-25 22:50:23 +0300

    lab3

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lena on 25.09.2021.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+// Created by lena on 25.09.2021.
+//
+
+#ifndef LAB3_TEST_H
+#define LAB3_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //LAB3_TEST_H
